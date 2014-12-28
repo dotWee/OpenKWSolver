3d637be891e70c2cdd93b9ebdc1f46c5a0198805
Lukas Wee <lukas.dot.wee@gmail.com>
2014-12-28 16:09:29 +0100

    Pull the API-Key from saved file, also in TextActivity

--- app/src/main/java/com/dotwee/openkwsolver/TextActivity.java
+++ app/src/main/java/com/dotwee/openkwsolver/TextActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class TextActivity extends ActionBarActivity {
@@ -32,5 +39,39 @@ public class TextActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // read the 9kw API-Key from previously saved file
+    private String pullKeyFromFile() {
+
+        String apikey = null;
+
+        try {
+
+            InputStream inputStream = openFileInput("apikey.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                apikey = stringBuilder.toString();
+            }
+
+        } catch (FileNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "Housten, couldn't find API-Key. You may want to enter it again.", Toast.LENGTH_LONG).show();
+            ;
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return apikey;
     }
 }
