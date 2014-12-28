d98a6ef832be2a280498ed6d90b2d922a341f376
Lukas Wee <lukas.dot.wee@gmail.com>
2014-12-28 16:04:07 +0100

    Re-introduce Dialog to get API-Key

--- app/src/main/java/com/dotwee/openkwsolver/MainActivity.java
+++ app/src/main/java/com/dotwee/openkwsolver/MainActivity.java
@@ -2,11 +2,20 @@ package com.dotwee.openkwsolver;
 
 import android.app.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends Activity {
 
@@ -47,6 +56,85 @@ public class MainActivity extends Activity {
     public void openConfirmActivity(View view) {
         Intent intent = new Intent(MainActivity.this, ConfirmActivity.class);
         startActivity(intent);
+    }
+
+    // dialog for api key
+    public void DialogKey() {
+        File MaybeExistingKey = new File("apikey.txt");
+        if (MaybeExistingKey.exists()) {
+            // ask kindly for the key
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setTitle("API-Key");
+            alert.setMessage("Please enter a working key here:");
+
+            final String keyfilename = "apikey.txt";
+            final EditText input_key = new EditText(this);
+            input_key.setText(pullKeyFromFile()); // TODO read the key from file
+
+            alert.setView(input_key);
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // save entered key as apikey.txt
+                    OutputStreamWriter save = null;
+                    try {
+                        save = new OutputStreamWriter(openFileOutput(keyfilename, MODE_APPEND));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    String apikey = input_key.getText().toString();
+                    try {
+                        save.write(apikey);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        save.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(getApplicationContext(), "Your API-Key is now saved!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            // ask kindly for the key
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setTitle("API-Key");
+            alert.setMessage("To use this application, you need an API-Key from 9kw.eu. Please enter it here:");
+
+            final String keyfilename = "apikey.txt";
+            final EditText input_key = new EditText(this);
+            input_key.setText(null);
+
+            alert.setView(input_key);
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // save entered key as apikey.txt
+                    OutputStreamWriter save = null;
+                    try {
+                        save = new OutputStreamWriter(openFileOutput(keyfilename, MODE_APPEND));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    String apikey = input_key.getText().toString();
+                    try {
+                        save.write(apikey);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        save.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(getApplicationContext(), "Your API-Key is now saved!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 
 }
