fabd8999607835993b007eb2310b00ecb8473146
Lukas Wee <lukas.dot.wee@gmail.com>
2015-03-08 18:07:51 +0100

    MainFragment: Give MainFragment its own layout

--- app/src/main/java/de/dotwee/openkwsolver/MainFragment.java
+++ app/src/main/java/de/dotwee/openkwsolver/MainFragment.java
@@ -65,7 +65,7 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
