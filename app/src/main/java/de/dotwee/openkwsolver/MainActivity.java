9c70160da33d142311810067701ef8eaab2aa262
Lukas Wee <lukas.dot.wee@gmail.com>
2015-03-08 19:51:03 +0100

    MainActivity: Remove Menu (no longer necessary)

--- app/src/main/java/de/dotwee/openkwsolver/MainActivity.java
+++ app/src/main/java/de/dotwee/openkwsolver/MainActivity.java
@@ -8,9 +8,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
     private static String LOG_TAG = "MainActivity";
@@ -47,29 +44,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_global, menu);
-        Log.i("onCreateOptionsMenu", "Return: " + true);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                // Todo move to settings
-                return true;
-            case R.id.action_stop:
-                finish();
-                System.exit(0);
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     @Override
