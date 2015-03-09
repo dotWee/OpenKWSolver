b945850238e94348dad04e11e9309426f74820a5
Lukas Wee <lukas.dot.wee@gmail.com>
2015-03-09 17:47:35 +0100

    SolverFragment: Disable savedInstanceState

--- app/src/main/java/de/dotwee/openkwsolver/Fragments/SolverFragment.java
+++ app/src/main/java/de/dotwee/openkwsolver/Fragments/SolverFragment.java
@@ -60,7 +60,7 @@ public class SolverFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_solver, container, true);
+        return inflater.inflate(R.layout.fragment_solver, container, false);
     }
 
     @Override
