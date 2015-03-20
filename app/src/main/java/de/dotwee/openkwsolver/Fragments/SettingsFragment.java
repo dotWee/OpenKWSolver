06aa84b4cf14c056d6921aff8b6e3d3cca75e739
Lukas Wee <lukas.dot.wee@gmail.com>
2015-03-20 17:30:25 +0100

    SettingsFragment: Enable buttons only if API-Key exist

--- app/src/main/java/de/dotwee/openkwsolver/Fragments/SettingsFragment.java
+++ app/src/main/java/de/dotwee/openkwsolver/Fragments/SettingsFragment.java
@@ -65,7 +65,12 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
 				}
 				return false;
 
+			case "pref_api_key":
+				SolverFragment.buttonPull.setEnabled(newValue.toString() != null);
+				return false;
+
+			default:
+				return false;
 		}
-		return false;
 	}
 }
