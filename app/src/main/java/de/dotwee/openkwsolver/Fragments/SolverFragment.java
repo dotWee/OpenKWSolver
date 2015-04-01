d9f8e025db110056bf4dfaa7f3bfe733dc8b2fcc
Lukas Wee <lukas.dot.wee@gmail.com>
2015-04-01 22:52:19 +0200

    SolverFragment: Improve first-use

--- app/src/main/java/de/dotwee/openkwsolver/Fragments/SolverFragment.java
+++ app/src/main/java/de/dotwee/openkwsolver/Fragments/SolverFragment.java
@@ -46,9 +46,9 @@ import de.dotwee.openkwsolver.Tools.DownloadImageTask;
 import de.dotwee.openkwsolver.Tools.StaticHelpers;
 
 public class SolverFragment extends Fragment {
-    public static final String URL_9WK = "http://www.9kw.eu:80/index.cgi";
-    public static final String URL_PARAMETER_CAPTCHA_SHOW = "?action=usercaptchashow";
-    public static final String URL_PARAMETER_SOURCE = "&source=androidopenkws";
+	public static final String URL_9WK = "http://www.9kw.eu:80/index.cgi";
+	public static final String URL_PARAMETER_CAPTCHA_SHOW = "?action=usercaptchashow";
+	public static final String URL_PARAMETER_SOURCE = "&source=androidopenkws";
 	private static final String LOG_TAG = "SolverFragment";
 
 	// main widgets
@@ -65,197 +65,199 @@ public class SolverFragment extends Fragment {
 	private Vibrator vibrator;
 	private Thread BalanceUpdate;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_solver, container, false);
-    }
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_solver, container, false);
+	}
 
-    @Override
-    public void onViewCreated(final View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-	    Log.i(LOG_TAG, "onViewCreated");
+	@Override
+	public void onViewCreated(final View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		Log.i(LOG_TAG, "onViewCreated");
 
-        // init prefs
-	    final SharedPreferences prefs = PreferenceManager
-			    .getDefaultSharedPreferences(getActivity());
+		// init prefs
+		final SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getActivity());
 
-        // declare main widgets
-	    buttonPull = (Button) view.findViewById(R.id.buttonPull);
-	    buttonSkip = (Button) view.findViewById(R.id.buttonSkip);
-	    buttonSend = (Button) view.findViewById(R.id.buttonSend);
+		// declare main widgets
+		buttonPull = (Button) view.findViewById(R.id.buttonPull);
+		buttonSkip = (Button) view.findViewById(R.id.buttonSkip);
+		buttonSend = (Button) view.findViewById(R.id.buttonSend);
 
-	    textViewCaptchaDesc = (TextView) view.findViewById(R.id.textViewDescID);
-	    textViewCaptcha = (TextView) view.findViewById(R.id.textViewID);
+		buttonSkip.setEnabled(false);
+		buttonSend.setEnabled(false);
 
-	    imageViewCaptcha = (ImageView) view.findViewById(R.id.imageViewCaptcha);
-	    // imageViewCaptcha.getLayoutParams().height = Integer.parseInt(prefs.getString("pref_layout_size", "200"));
+		textViewCaptchaDesc = (TextView) view.findViewById(R.id.textViewDescID);
+		textViewCaptcha = (TextView) view.findViewById(R.id.textViewID);
 
-	    editTextAnswer = (EditText) view.findViewById(R.id.editTextAnswer);
-	    progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+		imageViewCaptcha = (ImageView) view.findViewById(R.id.imageViewCaptcha);
+		// imageViewCaptcha.getLayoutParams().height = Integer.parseInt(prefs.getString("pref_layout_size", "200"));
 
-	    // hide captchaid textviews if disabled
-	    if (!StaticHelpers.isCaptchaIDEnabled(getActivity())) {
-		    textViewCaptchaDesc.setVisibility(View.GONE);
-	    }
-	    textViewCaptcha.setVisibility(View.GONE);
+		editTextAnswer = (EditText) view.findViewById(R.id.editTextAnswer);
+		progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 
-        // fix edittext width
-        editTextAnswer.setMaxWidth(editTextAnswer.getWidth());
+		// hide captchaid textviews if disabled
+		if (!StaticHelpers.isCaptchaIDEnabled(getActivity())) {
+			textViewCaptchaDesc.setVisibility(View.GONE);
+		}
+		textViewCaptcha.setVisibility(View.GONE);
 
-        // start showing balance if network and apikey is available
-	    if (StaticHelpers.isNetworkAvailable(getActivity())) {
-		    if (StaticHelpers.getApiKey(getActivity()) != null) {
-			    if (StaticHelpers.isAutoBalanceEnabled(getActivity())) {
-				    Log.i(LOG_TAG, "onCreated: start balance thread");
-				    balanceThread();
-			    }
-		    } else {
-			    Toast.makeText(getActivity(), "Set a API-Key to start", Toast.LENGTH_SHORT).show();
-		    }
-	    } else {
-		    Log.w(LOG_TAG, "No network available");
-		    Toast.makeText(getActivity(), "No network available!", Toast.LENGTH_SHORT).show();
-	    }
+		// fix edittext width
+		editTextAnswer.setMaxWidth(editTextAnswer.getWidth());
 
-	    // is no api key is set, disable the start button
-	    if (StaticHelpers.getApiKey(getActivity()) == null) {
-		    if (getView() != null) {
-			    buttonSkip.setEnabled(false);
-			    buttonSend.setEnabled(false);
-			    buttonPull.setEnabled(false);
-		    }
-	    }
+		// start showing balance if network and apikey is available
+		if (StaticHelpers.isNetworkAvailable(getActivity())) {
+			if (StaticHelpers.getApiKey(getActivity()) != null) {
+				if (StaticHelpers.isAutoBalanceEnabled(getActivity())) {
+					Log.i(LOG_TAG, "onCreated: start balance thread");
+					balanceThread();
+				}
+			} else {
+				Toast.makeText(getActivity(), "Set a API-Key to start", Toast.LENGTH_SHORT).show();
+			}
+		} else {
+			Log.w(LOG_TAG, "No network available");
+			Toast.makeText(getActivity(), "No network available!", Toast.LENGTH_SHORT).show();
+		}
 
-        buttonPull.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-	            Log.i(LOG_TAG, "onClickPull: Click recognized");
-	            if (StaticHelpers.isNetworkAvailable(getActivity())) {
-		            updateBalance();
-		            final String CaptchaID = StaticHelpers.requestCaptchaID(getActivity(), // needed Context
-				            prefs.getBoolean("pref_automation_loop", false), // Loop: false / true
-				            2); // 2 = Normal
-		            textViewCaptcha.setText(CaptchaID);
+		buttonPull.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (StaticHelpers.getApiKey(getActivity()) != null) {
+					buttonSend.setEnabled(true);
+					buttonSkip.setEnabled(true);
 
-		            // request captcha image
-		            CURRENT_CAPTCHA = pullCaptchaPicture(CaptchaID);
-		            buttonPull.setEnabled(false);
+					Log.i(LOG_TAG, "onClickPull: Click recognized");
+					if (StaticHelpers.isNetworkAvailable(getActivity())) {
+						updateBalance();
+						final String CaptchaID = StaticHelpers.requestCaptchaID(getActivity(), // needed Context
+								prefs.getBoolean("pref_automation_loop", false), // Loop: false / true
+								2); // 2 = Normal
+						textViewCaptcha.setText(CaptchaID);
 
-		            final int[] i = {0};
-		            final CountDownTimer CountDownTimer;
-		            CountDownTimer = new CountDownTimer(30000, 1000) {
+						// request captcha image
+						CURRENT_CAPTCHA = pullCaptchaPicture(CaptchaID);
+						buttonPull.setEnabled(false);
 
-			            @Override
-			            public void onTick(long millisUntilFinished) {
-				            i[0]++;
-				            progressBar.setProgress(i[0]);
-			            }
+						final int[] i = {0};
+						final CountDownTimer CountDownTimer;
+						CountDownTimer = new CountDownTimer(30000, 1000) {
 
-			            @Override
-			            public void onFinish() {
-				            StaticHelpers.skipCaptchaByID(getActivity(), CaptchaID);
-			            }
-		            };
+							@Override
+							public void onTick(long millisUntilFinished) {
+								i[0]++;
+								progressBar.setProgress(i[0]);
+							}
 
-		            buttonSkip = (Button) view.findViewById(R.id.buttonSkip);
-		            buttonSkip.setOnClickListener(new View.OnClickListener() {
-			            @Override
-			            public void onClick(View v1) {
-				            Log.i("OnClickSkip", "Click recognized");
-				            editTextAnswer.setText(null);
-				            StaticHelpers.skipCaptchaByID(
-						            getActivity(), StaticHelpers.getApiKey(getActivity()));
+							@Override
+							public void onFinish() {
+								StaticHelpers.skipCaptchaByID(getActivity(), CaptchaID);
+							}
+						};
 
-				            CountDownTimer.cancel();
-				            progressBar.setProgress(0);
+						buttonSkip = (Button) view.findViewById(R.id.buttonSkip);
+						buttonSkip.setOnClickListener(new View.OnClickListener() {
+							@Override
+							public void onClick(View v1) {
+								Log.i("OnClickSkip", "Click recognized");
+								editTextAnswer.setText(null);
+								StaticHelpers.skipCaptchaByID(
+										getActivity(), StaticHelpers.getApiKey(getActivity()));
 
-				            imageViewCaptcha.setImageDrawable(null);
+								CountDownTimer.cancel();
+								progressBar.setProgress(0);
 
-				            buttonPull.setEnabled(true);
-			            }
-		            });
+								imageViewCaptcha.setImageDrawable(null);
 
-		            if (CURRENT_CAPTCHA == true) {
+								buttonPull.setEnabled(true);
+							}
+						});
+
+						if (CURRENT_CAPTCHA == true) {
 
 
-			            vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-			            if (StaticHelpers.isVibrateEnabled(getActivity())) {
-				            vibrator.vibrate(500);
-			            }
+							vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+							if (StaticHelpers.isVibrateEnabled(getActivity())) {
+								vibrator.vibrate(500);
+							}
 
-			            CountDownTimer.start();
-			            buttonSend = (Button) view.findViewById(R.id.buttonSend);
-			            buttonSend.setOnClickListener(new View.OnClickListener() {
-				            @Override
-				            public void onClick(View v1) {
-					            String CaptchaAnswer = editTextAnswer.getText().toString();
-					            if (!CaptchaAnswer.equalsIgnoreCase("")) {
-						            StaticHelpers.sendCaptchaByID(getActivity(), CaptchaID, CaptchaAnswer, false);
+							CountDownTimer.start();
+							buttonSend = (Button) view.findViewById(R.id.buttonSend);
+							buttonSend.setOnClickListener(new View.OnClickListener() {
+								@Override
+								public void onClick(View v1) {
+									String CaptchaAnswer = editTextAnswer.getText().toString();
+									if (!CaptchaAnswer.equalsIgnoreCase("")) {
+										StaticHelpers.sendCaptchaByID(getActivity(), CaptchaID, CaptchaAnswer, false);
 
-						            CountDownTimer.cancel();
-						            Log.i("OnClickSend", "Timer killed");
-						            progressBar.setProgress(0);
+										CountDownTimer.cancel();
+										Log.i("OnClickSend", "Timer killed");
+										progressBar.setProgress(0);
 
-						            imageViewCaptcha.setImageDrawable(null);
-						            editTextAnswer.setText(null);
+										imageViewCaptcha.setImageDrawable(null);
+										editTextAnswer.setText(null);
 
-						            if (StaticHelpers.isLoopEnabled(getActivity())) {
-							            Log.i("OnClickSend", "Loop-Mode");
-							            buttonPull.performClick();
-						            } else {
-							            buttonPull.setEnabled(true);
-						            }
-					            } else {
-						            Toast.makeText(getActivity(),
-								            R.string.main_toast_emptyanswer, Toast.LENGTH_LONG).show();
-					            }
-				            }
-			            });
-		            } else {
-			            buttonSkip.performClick();
-		            }
+										if (StaticHelpers.isLoopEnabled(getActivity())) {
+											Log.i("OnClickSend", "Loop-Mode");
+											buttonPull.performClick();
+										} else {
+											buttonPull.setEnabled(true);
+										}
+									} else {
+										Toast.makeText(getActivity(),
+												R.string.main_toast_emptyanswer, Toast.LENGTH_LONG).show();
+									}
+								}
+							});
+						} else {
+							buttonSkip.performClick();
+						}
 
-	            } else {
-		            Log.w(LOG_TAG, "onClickPull: Click without network");
-		            Toast.makeText(getActivity(), "No network available!", Toast.LENGTH_SHORT).show();
-	            }
-            }
-        });
-    }
+					} else {
+						Log.w(LOG_TAG, "onClickPull: Click without network");
+						Toast.makeText(getActivity(), "No network available!", Toast.LENGTH_SHORT).show();
+					}
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+				} else {
+					Toast.makeText(getActivity(), "Set an API-Key first!", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+	}
 
-	    if (BalanceUpdate != null) {
-		    if (BalanceUpdate.isAlive()) {
-			    BalanceUpdate.interrupt();
-		    }
-	    }
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
 
-    }
+		if (BalanceUpdate != null) {
+			if (BalanceUpdate.isAlive()) {
+				BalanceUpdate.interrupt();
+			}
+		}
+
+	}
 
 	// Pull Captcha picture and display it
 	public boolean pullCaptchaPicture(String CaptchaID) {
-        String CaptchaPictureURL = (URL_9WK + URL_PARAMETER_CAPTCHA_SHOW +
-		        URL_PARAMETER_SOURCE + StaticHelpers.getExternalParameter(getActivity(), 2) +
-		        "&id=" + CaptchaID + StaticHelpers.getApiKey(getActivity()));
+		String CaptchaPictureURL = (URL_9WK + URL_PARAMETER_CAPTCHA_SHOW +
+				URL_PARAMETER_SOURCE + StaticHelpers.getExternalParameter(getActivity(), 2) +
+				"&id=" + CaptchaID + StaticHelpers.getApiKey(getActivity()));
 
-        Log.i("pullCaptchaPicture", "URL: " + CaptchaPictureURL);
-        if (getView() != null) {
-	        try {
-		        Bitmap returnBit = new DownloadImageTask(imageViewCaptcha).execute(CaptchaPictureURL).get(5000, TimeUnit.MILLISECONDS);
-		        if (returnBit != null) {
-			        return true; // true = new image
-		        }
-	        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-		        e.printStackTrace();
-	        }
-        }
+		Log.i("pullCaptchaPicture", "URL: " + CaptchaPictureURL);
+		if (getView() != null) {
+			try {
+				Bitmap returnBit = new DownloadImageTask(imageViewCaptcha).execute(CaptchaPictureURL).get(5000, TimeUnit.MILLISECONDS);
+				if (returnBit != null) {
+					return true; // true = new image
+				}
+			} catch (InterruptedException | ExecutionException | TimeoutException e) {
+				e.printStackTrace();
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
 	public void updateBalance() {
 		if (getView() != null) {
@@ -264,38 +266,41 @@ public class SolverFragment extends Fragment {
 		}
 	}
 
-    // BalanceThread: Update the balance every 5 seconds
-    public void balanceThread() {
-        BalanceUpdate = new Thread() {
+	// BalanceThread: Update the balance every 5 seconds
+	public void balanceThread() {
+		BalanceUpdate = new Thread() {
 
-            @Override
-            public void run() {
-                while (!isInterrupted()) {
-                    try {
-                        Thread.sleep(5000); // 5000ms = 5s
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (getView() != null) {
-	                            textViewBalance = (TextView) getView()
-			                            .findViewById(R.id.textViewBA);
-	                            textViewBalance.setText(StaticHelpers.getBalance(getActivity()));
-                            }
-                        }
-                    });
-                }
+			@Override
+			public void run() {
+				while (!isInterrupted()) {
+					try {
+						Thread.sleep(5000); // 5000ms = 5s
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					getActivity().runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							if (getView() != null) {
+								textViewBalance = (TextView) getView()
+										.findViewById(R.id.textViewBA);
+								textViewBalance.setText(StaticHelpers.getBalance(getActivity()));
+							}
+						}
+					});
+				}
 
-            }
-        };
+			}
+		};
 
-        // check if thread isn't already running.
-        if (BalanceUpdate.isAlive())
-            BalanceUpdate.interrupt();
+		// check if thread isn't already running.
+		if (BalanceUpdate.isAlive()) {
+			BalanceUpdate.interrupt();
+		}
 
-            // if not, start it
-        else BalanceUpdate.start();
-    }
+		// if not, start it
+		else {
+			BalanceUpdate.start();
+		}
+	}
 }
