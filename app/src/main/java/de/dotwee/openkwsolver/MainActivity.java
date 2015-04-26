8a6743665af99e4a7bb444c4ebb16e9aff871542
Lukas Wee <lukas.dot.wee@gmail.com>
2015-04-26 17:32:12 +0200

    MainActivity: No need to setup the actionbar

--- app/src/main/java/de/dotwee/openkwsolver/MainActivity.java
+++ app/src/main/java/de/dotwee/openkwsolver/MainActivity.java
@@ -22,9 +22,7 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -34,17 +32,11 @@ import de.dotwee.openkwsolver.Fragments.SolverFragment;
 
 public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
 	private static final String LOG_TAG = "MainActivity";
-	private ActionBar actionBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			actionBar.setLogo(R.mipmap.ic_launcher);
-			actionBar.setTitle("  " + getResources().getString(R.string.app_name));
-		}
 
 
 	    if (!getResources().getBoolean(R.bool.isTablet)) {
@@ -72,16 +64,12 @@ public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuIt
 	}
 
 	public class FragmentAdapter extends FragmentPagerAdapter {
-		private static final String LOG_TAG = "FragmentAdapter";
-
 		public FragmentAdapter(FragmentManager fm) {
 			super(fm);
         }
 
         @Override
         public Fragment getItem(int position) {
-			Log.i(LOG_TAG, "getItem / " + position);
-
 	        switch (position) {
 		        case 0: return new SolverFragment();
 		        case 1: return new SettingsFragment();
